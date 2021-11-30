8f0a92a9b54b15bb8b8b387a5342483efe58b084
kacarato9 <kacarato9@gmail.com>
2021-11-30 15:47:47 -0500

    Stage 3 functionality added

--- board.py
+++ board.py
@@ -1,9 +1,92 @@
-board = ['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']
-
 def p_board():
     print(board[0])
     print(board[1])
     print(board[2])
+
+
+def full():
+    for i in board:
+        for x in i:
+            if x == '-':
+                return False
+    print("Tied game!")
+    quit()
+
+
+def check_win1():
+    for i in range(length):
+        win = True
+        for x in range(length):
+            if board[i][x] != "X":
+                win = False
+                break
+        if win:
+            print("player1 wins!")
+            quit()
+    for i in range(length):
+        win = True
+        for x in range(length):
+            if board[x][i] != "X":
+                win = False
+                break
+        if win:
+            print("player1 wins!")
+            quit()
+
+    win = True
+    for i in range(length):
+        if board[i][i] != "X":
+            win = False
+            break
+    if win:
+        print("player1 wins!")
+        quit()
+    win = True
+    for i in range(length):
+        if board[i][length - 1 - i] != "X":
+            win = False
+            break
+    if win:
+        print("player1 wins!")
+        quit()
+
+
+def check_win2():
+    for i in range(length):
+        win = True
+        for x in range(length):
+            if board[i][x] != "O":
+                win = False
+                break
+        if win:
+            print("player2 wins!")
+            quit()
+    for i in range(length):
+        win = True
+        for x in range(length):
+            if board[x][i] != "O":
+                win = False
+                break
+        if win:
+            print("player2 wins!")
+            quit()
+    win = True
+    for i in range(length):
+        if board[i][i] != "O":
+            win = False
+            break
+    if win:
+        print("player2 wins!")
+        quit()
+    win = True
+    for i in range(length):
+        if board[i][length - 1 - i] != "O":
+            win = False
+            break
+    if win:
+        print("player2 wins!")
+        quit()
+
 
 def player1(board):
     print("Player 1, please make your move.")
@@ -24,6 +107,9 @@ def player1(board):
             print("row:", row, "col:", col, " has already been taken, please select again.")
             again = True
     p_board()
+    check_win1()
+    full()
+
 
 def player2(board):
     print("Player 2, please make your move.")
@@ -44,10 +130,15 @@ def player2(board):
             print("row:", row, "col:", col, " has already been taken, please select again.")
             again = True
     p_board()
+    check_win2()
+    full()
 
+
+board = ['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']
 p_board()
-for i in board:
-    for x in i:
-        if x == '-':
-            player1(board)
-            player2(board)
+length = len(board)
+while full() == False:
+    player1(board)
+    player2(board)
+
+
